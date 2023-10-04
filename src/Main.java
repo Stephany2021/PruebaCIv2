7174d13231522f8da7cdb78868c0c308ce6df07a
DESKTOP-7138OCQ\Stephany <stephany.duran1512@gmail.com>
2023-10-04 18:12:02 -0500

    Creación clase prueba

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("A commit has been made");
-
+        System.out.printf("A commit has been " + "made");
+        System.out.println("Hi Stephany");
 
     }
 }
---
+++ src/Prueba.java
@@ -0,0 +1,3 @@
+public class Prueba {
+    public String mensaje;
+}
